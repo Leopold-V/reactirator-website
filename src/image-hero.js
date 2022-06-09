4b0813e87a8924116d09c79fc256ae0404c2e062
Leopold-V <leopoldV79@gmail.com>
2022-06-09 21:43:12 +0200

    update

---
+++ src/image-hero.js
@@ -0,0 +1,33 @@
+let constrain = 30;
+let mouseOverContainer = document.querySelector(".container_hero_image");
+let ex1Layer = document.querySelector(".hero_image");
+
+function transforms(x, y, el) {
+  let box = el.getBoundingClientRect();
+  let calcX = -(y - box.y - box.height / 2) / constrain;
+  let calcY = (x - box.x - box.width / 2) / constrain;
+
+  return (
+    "perspective(200px) " +
+    "   rotateX(" +
+    calcX +
+    "deg) " +
+    "   rotateY(" +
+    calcY +
+    "deg) "
+  );
+}
+
+function transformElement(el, xyEl) {
+  el.style.transform = transforms.apply(null, xyEl);
+}
+
+mouseOverContainer.onmousemove = function (e) {
+  let xy = [e.clientX, e.clientY];
+  let position = xy.concat([ex1Layer]);
+
+  console.log(position);
+  window.requestAnimationFrame(function () {
+    transformElement(ex1Layer, position);
+  });
+};
